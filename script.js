7ccfee7ba4f2f9cc5c2012166679aacdf194f6a6
Chanry Chen <chanryc9471@hstat.org>
2023-03-20 21:07:54 -0400

    start kaboom

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// start the game
+kaboom({
+    background: [0, 0, 0]
+})
 
+// sprites
+loadSprite('cat', 'img/_.png')
+loadSprite('enemy', 'img/_.png')
+loadSprite('octo-coin', 'img/_.png')
+loadSprite('orange-block', 'img/_.png')
+loadSprite('yellow-block', 'img/_.png')
+loadSprite('green-block', 'img/_.png')
+loadSprite('blue-block', 'img/_.png')
+loadSprite()
